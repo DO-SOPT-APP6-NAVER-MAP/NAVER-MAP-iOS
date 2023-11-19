cf0cebf54884224d0e0845dba659d06cedcde2ce
Daye Kim <dykim315@naver.com>
2023-11-20 00:56:49 +0900

    [Setting] #1 - 폴더링

---
+++ NAVER-MAP-iOS/Global/Consts/FontLiterals.swift
@@ -0,0 +1,8 @@
+//
+//  FontLiterals.swift
+//  NAVER-MAP-iOS
+//
+//  Created by 김다예 on 11/20/23.
+//
+
+import Foundation
---
+++ NAVER-MAP-iOS/Global/Consts/ImageLiterals.swift
@@ -0,0 +1,8 @@
+//
+//  ImageLiterals.swift
+//  NAVER-MAP-iOS
+//
+//  Created by 김다예 on 11/20/23.
+//
+
+import Foundation
