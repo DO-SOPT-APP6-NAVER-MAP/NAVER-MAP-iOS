f2c2a04fbc6189c36b19cdfeade1aecce719bb44
0seoYun <yys020819@gmail.com>
2023-11-23 05:05:17 +0900

    [Feat] #3 - header 연결 및 컬렉션뷰 섹션 구획

--- NAVER-MAP-iOS/Screen/Detail/DetailViewController.swift
+++ NAVER-MAP-iOS/Screen/Detail/DetailViewController.swift
@@ -8,22 +8,119 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    // MARK: - UI Properties
+    
+    private lazy var detailCollectionView: UICollectionView = { createCollectionView() }()
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+        setupLayout()
+        setupCollectionViewConfig()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Private Method
+
+private extension DetailViewController {
+    
+    func setupViews() {
+        self.navigationController?.isNavigationBarHidden = true
+        view.addSubviews([detailCollectionView])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupLayout() {
+        detailCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
-    */
-
+    
+    func setupCollectionViewConfig() {
+        self.detailCollectionView.register(DetailMainSectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DetailMainSectionHeaderView.identifier)
+        
+        self.detailCollectionView.delegate = self
+        self.detailCollectionView.dataSource = self
+    }
+    
+    // MARK: - Method for creating UI properties
+    
+    func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            if sectionIndex == 0 {
+                return self.createFirstSectionLayout()
+            } else {
+                return self.createFirstSectionLayout()
+            }
+        }
+    }
+    
+    func createFirstSectionLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(0)) // 아이템 없음
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [self.createSectionHeader()]
+        return section
+    }
+    
+    func createOtherSectionLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(0)) // 아이템 없음
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [self.createSectionHeader()]
+        return section
+    }
+    
+    func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        return header
+    }
+    
+    func createCollectionView() -> UICollectionView {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = UIColor.naverMapWhite
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }
 }
+
+// MARK: - CollectionView DataSource
+
+extension DetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader else {
+            return UICollectionReusableView()
+        }
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailMainSectionHeaderView.identifier, for: indexPath) as! DetailMainSectionHeaderView
+        return headerView
+    }
+}
+
+// MARK: - CollectionVeiw Delegate
+
+extension DetailViewController: UICollectionViewDelegate {
+    
+}
+
+
