76b50944688994d04d51bb2749a3fdd58175231a
0seoYun <yys020819@gmail.com>
2023-11-24 03:25:52 +0900

    [Add] #3 - Image Assets / Header View 추가

---
+++ NAVER-MAP-iOS/Screen/Detail/View/SupplementaryView/DescriptionSectionHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  DescriptionSectionHeaderView.swift
+//  NAVER-MAP-iOS
+//
+//  Created by 윤영서 on 11/23/23.
+//
+
+import UIKit
+
+class DescriptionSectionHeaderView: UICollectionReusableView {
+        
+}
