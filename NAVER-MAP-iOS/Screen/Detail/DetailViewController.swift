8c37609259e3513fb485594272c93b57b17ed797
0seoYun <yys020819@gmail.com>
2023-11-24 03:34:21 +0900

    [Feat] #3 - 두번째 섹션 헤더 연결 및 수정

--- NAVER-MAP-iOS/Screen/Detail/DetailViewController.swift
+++ NAVER-MAP-iOS/Screen/Detail/DetailViewController.swift
@@ -42,66 +42,67 @@ private extension DetailViewController {
     func setupCollectionViewConfig() {
         self.detailCollectionView.register(DetailMainSectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DetailMainSectionHeaderView.identifier)
         
+        self.detailCollectionView.register(DescriptionSectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DescriptionSectionHeaderView.identifier)
+        
         self.detailCollectionView.delegate = self
         self.detailCollectionView.dataSource = self
     }
     
-    // MARK: - Method for creating UI properties
-    
-    func createLayout() -> UICollectionViewLayout {
-        return UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
-            if sectionIndex == 0 {
-                return self.createFirstSectionLayout()
-            } else {
-                return self.createFirstSectionLayout()
-            }
-        }
-    }
-    
-    func createFirstSectionLayout() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(0)) // 아이템 없음
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.boundarySupplementaryItems = [self.createSectionHeader()]
-        return section
-    }
-    
-    func createOtherSectionLayout() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(0)) // 아이템 없음
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.boundarySupplementaryItems = [self.createSectionHeader()]
-        return section
-    }
-    
-    func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
-        let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0))
-        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
-        return header
-    }
+    // MARK: - Create UI properties Method
     
     func createCollectionView() -> UICollectionView {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: configLayout())
         collectionView.backgroundColor = UIColor.naverMapWhite
         collectionView.showsVerticalScrollIndicator = false
         collectionView.showsHorizontalScrollIndicator = false
         return collectionView
     }
+    
+    func createSectionHeader(forSection section: Int) -> NSCollectionLayoutBoundarySupplementaryItem {
+        let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(507))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        
+        return header
+    }
+    
+    // MARK: - Configure Layout Method
+    
+    func configLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            if sectionIndex == 0 || sectionIndex == 1 {
+                return self.configHeaderSectionLayout(forSection: sectionIndex)
+            } else {
+                return nil // 나중에 config other section layout
+            }
+        }
+    }
+    
+    func configHeaderSectionLayout(forSection section: Int) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(0)) // 아이템 없음
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let sectionLayout = NSCollectionLayoutSection(group: group)
+        let header = createSectionHeader(forSection: section)
+        sectionLayout.boundarySupplementaryItems = [header]
+        return sectionLayout
+    }
+    
+    // TODO: - 추후 레이아웃 구현
+
 }
 
 // MARK: - CollectionView DataSource
 
-extension DetailViewController: UICollectionViewDataSource {
+extension DetailViewController: UICollectionViewDataSource {    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        if section == 0 || section == 1 {
+            return 0
+        } else {
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -112,8 +113,26 @@ extension DetailViewController: UICollectionViewDataSource {
         guard kind == UICollectionView.elementKindSectionHeader else {
             return UICollectionReusableView()
         }
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailMainSectionHeaderView.identifier, for: indexPath) as! DetailMainSectionHeaderView
-        return headerView
+        
+        if indexPath.section == 0 {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailMainSectionHeaderView.identifier, for: indexPath) as? DetailMainSectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            return headerView
+            
+        } else if indexPath.section == 1 {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DescriptionSectionHeaderView.identifier, for: indexPath) as? DescriptionSectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            return headerView
+            
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 6
     }
 }
 
@@ -122,5 +141,6 @@ extension DetailViewController: UICollectionViewDataSource {
 extension DetailViewController: UICollectionViewDelegate {
     
 }
+
 
 
