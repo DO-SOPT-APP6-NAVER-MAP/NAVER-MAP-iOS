4d530191cc773a684d7204236704653009754c89
seuriseuljjeok <heeseul09@naver.com>
2023-12-01 17:41:42 +0900

    [Setting] naver-map-ios SDK 세팅

--- NAVER-MAP-iOS/Global/Supporting Files/Config.swift
+++ NAVER-MAP-iOS/Global/Supporting Files/Config.swift
@@ -11,6 +11,8 @@ enum Config {
     enum Keys {
         enum Plist {
             static let baseURL = "BASE_URL"
+            static let clientId = "CLIENT_ID"
+            static let apiKey = "API_KEY"
         }
     }
     
